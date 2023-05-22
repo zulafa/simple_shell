7c44590bb0ebb2edae3d75bfd0f8fbe1a0c7feb3
zulafa <zulafamaalim@gmail.com>
2023-05-22 02:12:23 -0700

    getenv.c

---
+++ getenv.c
@@ -0,0 +1,96 @@
+#include "shell.h"
+
+/**
+ * getenv - this function is used to
+ * retrieve the value of an environment variable.
+ * @info: rep the structure that is used to maintain
+ * constant function prototype.
+ * Return: will always return 0 if the environ
+ * can not be retrieved.
+ */
+char **getenv(info_t *info)
+{
+	if (!info->environ || info->env_changed)
+	{
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
+	}
+	return (info->environ);
+}
+/**
+ * _unsetenv - refers to a function in C that is used
+ * to remove an environment variable.
+ * @info: rep the structure that is used to maintain
+ * constant function prototype.
+ * Return: will return 1 on delete, and 0 otherwise.
+ * @var: rep the environment variable property.
+ */
+
+int _unsetenv(info_t *info, char *var)
+{
+	list_t *node = info->env;
+	size_t i = 0;
+	char *p;
+
+	if (!node || !var)
+		return (0);
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			info->env_changed = delete_node_at_index(&(info->env), i);
+			i = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		i++;
+	}
+	return (info->env_changed);
+}
+
+/**
+ * _setenv - refers to a function in C that is used to set
+ * the value of an environment variable. Takes two arguments:
+ * the name of the variable and its value.
+ * @info: rep the structure that is used
+ * to maintain constant function prototype.
+ * @var: refers to the variable of the environment.
+ * @value: refers to the value taken in by the environment.
+ * Return: will always return 0
+ */
+int _setenv(info_t *info, char *var, char *value)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *p;
+
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = info->env;
+
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(info->env), buf, 0);
+	free(buf);
+	info->env_changed = 1;
+	return (0);
+}
