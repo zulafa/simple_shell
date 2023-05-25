995eb6d43763fe986c4598f682a763f52ac1c781
zulafa <zulafamaalim@gmail.com>
2023-05-25 05:27:56 -0700

    parser.c

---
+++ parser.c
@@ -0,0 +1,88 @@
+#include "shell.h"
+
+/**
+ * is_cmd - function that will determine if a file is an
+ * executable command.
+ * @info: rep the info struct
+ * @path: rep the path to the file.
+ *
+ * Return: will return 1 if its true, and 0 otherwise.
+ */
+int is_cmd(info_t *info, char *path)
+{
+	struct stat st;
+
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * dup_chars - rep for duplicating characters.
+ * @pathstr: rep the PATH string
+ * @start: rep the starting index
+ * @stop: rep the stopping index
+ *
+ * Return: will return a pointer to the new buffer
+ */
+char *dup_chars(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int i = 0, k = 0;
+
+	for (k = 0, i = start; i < stop; i++)
+		if (pathstr[i] != ':')
+			buf[k++] = pathstr[i];
+	buf[k] = 0;
+	return (buf);
+}
+
+/**
+ * find_path - responsible fo finding this cmd in the PATH string
+ * @info: rep the info struct
+ * @pathstr: rep the PATH string
+ * @cmd: rep the cmd that needs to be found
+ *
+ * Return: will return the full path of cmd if its found and NULL
+ * if it aint found.
+ */
+char *find_path(info_t *info, char *pathstr, char *cmd)
+{
+	int i = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	{
+		if (is_cmd(info, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[i] || pathstr[i] == ':')
+		{
+			path = dup_chars(pathstr, curr_pos, i);
+			if (!*path)
+				_strcat(path, cmd);
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (is_cmd(info, path))
+				return (path);
+			if (!pathstr[i])
+				break;
+			curr_pos = i;
+		}
+		i++;
+	}
+	return (NULL);
+}
